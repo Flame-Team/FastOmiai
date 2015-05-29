76a7436dbb823c167ea0d42cff98926100aa1dc3
meng85440835 <85440835@qq.com>
2015-05-29 20:48:16 +0800

    实现推荐列表

--- FastOmiai/src/com/sogou/fastomiai/BrowseActivity.java
+++ FastOmiai/src/com/sogou/fastomiai/BrowseActivity.java
@@ -1,5 +1,17 @@
 package com.sogou.fastomiai;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.sogou.fastomiai.controller.SessionManager;
+import com.sogou.fastomiai.model.FindListInfo;
+import com.sogou.fastomiai.util.Constants;
+import com.sogou.fastomiai.util.NetworkRequest;
+import com.sogou.fastomiai.util.NetworkUtil;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -21,8 +33,9 @@ public class BrowseActivity extends FragmentActivity {
     private PagerAdapter mPagerAdapter;
     
     private Button mBtnHomePage = null;
-    private Button mBtnFilter = null;
-
+    private Button mBtnFilter = null; 
+    private FindListInfo mFindListInfo;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,25 +60,69 @@ public class BrowseActivity extends FragmentActivity {
 				Toast.makeText(getApplicationContext(), "筛选", Toast.LENGTH_SHORT).show();
 			}
 		});
-
+        getFindListData();
         mPager = (ViewPager) findViewById(R.id.pager);
-        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
-        mPager.setAdapter(mPagerAdapter);
+       
+      
     }
 
     private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-        public ScreenSlidePagerAdapter(FragmentManager fm) {
+        
+      
+    	public ScreenSlidePagerAdapter(FragmentManager fm) {
             super(fm);
+
         }
 
         @Override
         public Fragment getItem(int position) {
-            return BrowseFragment.create(getApplicationContext(), position);
+            return BrowseFragment.create(getApplicationContext(), position, mFindListInfo);
         }
 
         @Override
         public int getCount() {
             return NUM_PAGES;
         }
+    }
+    
+    public void getFindListData()
+    {
+    	int nStart = 0;
+    	int nLimit = 10;
+    	
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("start", String.valueOf(nStart));
+        params.put("limit", String.valueOf(nLimit));
+        params.put("token",  SessionManager.getInstance(getApplicationContext()).getToken());
+        String url = NetworkUtil.getUrl(Constants.FIND_LIST_URL, params);
+       
+        NetworkRequest.get(url, FindListInfo.class,
+                new Response.Listener<FindListInfo>() {
+                    @Override
+                    public void onResponse(FindListInfo findListInfo) {
+                    	
+                        if (findListInfo != null)
+                        {
+                        	mFindListInfo = findListInfo;
+//                        	mListTags = findListInfo.data.get(0).tags;
+//                        	mStrUrl = findListInfo.data.get(0).headUrl;
+                        	if (findListInfo.isSuccess()) {
+                        		mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+                            	mPager.setAdapter(mPagerAdapter);
+							}
+                        }
+                        else 
+                        {
+                        	
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {  
+                        
+                    }
+                },
+                false);
     }
 }
--- FastOmiai/src/com/sogou/fastomiai/BrowseFragment.java
+++ FastOmiai/src/com/sogou/fastomiai/BrowseFragment.java
@@ -1,14 +1,20 @@
 package com.sogou.fastomiai;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.android.volley.toolbox.NetworkImageView;
+import com.sogou.fastomiai.model.FindListInfo;
+import com.sogou.fastomiai.util.NetworkRequest;
 
 /**
  * A fragment representing a single step in a wizard. The fragment shows a dummy title indicating
@@ -30,20 +36,33 @@ public class BrowseFragment extends Fragment {
     
     private Context mContext;
     private Button mBtnDate = null;
-
+    private ArrayList<Button> listBtn = new ArrayList<Button>();
+    private Button mBtnTag1 = null;
+    private Button mBtnTag2 = null;
+    private Button mBtnTag3 = null;
+    private Button mBtnTag4 = null;
+    private Button mBtnTag5 = null;
+    private ArrayList<String> mListTags;
+    private String mStrUrl;
+    private FindListInfo mFindListInfo;
+    
+    private NetworkImageView netImageView; 
+    
     /**
      * Factory method for this fragment class. Constructs a new fragment for the given page number.
      */
-    public static BrowseFragment create(Context context, int pageNumber) {
-    	BrowseFragment fragment = new BrowseFragment(context);
+    public static BrowseFragment create(Context context, int pageNumber, FindListInfo findListInfo) {
+    	BrowseFragment fragment = new BrowseFragment(context, findListInfo);
         Bundle args = new Bundle();
         args.putInt(ARG_PAGE, pageNumber);
         fragment.setArguments(args);
+        
         return fragment;
     }
 
-    public BrowseFragment(Context context) {
+    public BrowseFragment(Context context, FindListInfo findListInfo) {
     	mContext = context;
+    	mFindListInfo = findListInfo;
     }
 
     @Override
@@ -58,6 +77,31 @@ public class BrowseFragment extends Fragment {
 
         ViewGroup rootView = (ViewGroup) inflater
                 .inflate(R.layout.fragment_browse, container, false);
+        
+        if (mFindListInfo.data.size() <= mPageNumber) {
+			return null;
+		}
+        
+        mStrUrl = mFindListInfo.data.get(mPageNumber).headUrl;
+        mListTags = mFindListInfo.data.get(mPageNumber).tags;
+        
+        NetworkImageView imageHead = (NetworkImageView)rootView.findViewById(R.id.image_user_photo);
+        imageHead.setImageUrl(mStrUrl, NetworkRequest.getInstance(mContext.getApplicationContext()).getImageLoader());
+        mBtnTag1 = (Button) rootView.findViewById(R.id.text_tag1);
+        mBtnTag2 = (Button) rootView.findViewById(R.id.text_tag2);
+        mBtnTag3 = (Button) rootView.findViewById(R.id.text_tag3);
+        mBtnTag4 = (Button) rootView.findViewById(R.id.text_tag4);
+        mBtnTag5 = (Button) rootView.findViewById(R.id.text_tag5);
+        listBtn.add(mBtnTag1);
+        listBtn.add(mBtnTag2);
+        listBtn.add(mBtnTag3);
+        listBtn.add(mBtnTag4);
+        listBtn.add(mBtnTag5);
+        for(int i=0; i<mListTags.size(); i++)
+        {
+        	listBtn.get(i).setText(mListTags.get(i));
+        	listBtn.get(i).setVisibility(View.VISIBLE);
+        }
         
         mBtnDate = (Button) rootView.findViewById(R.id.btn_browse_date);
         mBtnDate.setOnClickListener(new OnClickListener() {
@@ -75,4 +119,8 @@ public class BrowseFragment extends Fragment {
     public int getPageNumber() {
         return mPageNumber;
     }
+    
+
+    
+
 }
